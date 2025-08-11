b83481083aee8eca9e826d04fdc42b6258a062f1
Ilya <ilya.zaharov.2009@mail.ru>
2025-08-11 19:55:02 +0300

    fix

--- Jam squad/Assets/Scripts/CellManager.cs
+++ Jam squad/Assets/Scripts/CellManager.cs
@@ -1,7 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class CellManager : MonoBehaviour
 {
@@ -15,14 +18,21 @@ public class CellManager : MonoBehaviour
 
     [Header("Анимация поражения")]
     [SerializeField] private SmoothFollowCamera _camera;
-
-
+    [SerializeField] private Image fadeImage; // Чёрный Image на весь экран
+    [SerializeField] private float fadeDuration = 1.5f;
 
     private List<string> _availableMessages = new List<string>();
-    // Start is called before the first frame update
+
     private void Start()
     {
         _availableMessages = new List<string>(messageTextes);
+
+        // Начальная настройка затемнения
+        if (fadeImage != null)
+        {
+            fadeImage.color = new Color(0, 0, 0, 0);
+            fadeImage.gameObject.SetActive(false);
+        }
     }
 
     public void GetMemoryMessage()
@@ -30,19 +40,18 @@ public class CellManager : MonoBehaviour
         if (_availableMessages.Count == 0)
         {
             WinGame();
+            return;
         }
 
         int randomIndex = Random.Range(0, _availableMessages.Count);
         messageUI.text = _availableMessages[randomIndex];
         _availableMessages.RemoveAt(randomIndex);
         messageObj.gameObject.SetActive(true);
-
     }
 
     private void WinGame()
     {
         winMenu.SetActive(true);
-
     }
 
     public void LoosGame(GameObject cell)
@@ -50,5 +59,25 @@ public class CellManager : MonoBehaviour
         _camera.target = cell.transform;
         PlayerHolder player = FindAnyObjectByType<PlayerHolder>();
         Destroy(player.gameObject);
+
+        StartCoroutine(FadeAndLose());
+    }
+
+    private IEnumerator FadeAndLose()
+    {
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            fadeImage.DOFade(1f, fadeDuration).SetEase(Ease.InOutQuad)
+            .OnComplete(() =>
+             {
+                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+             });
+        }
+
+        yield return new WaitForSeconds(fadeDuration);
+
+        // Здесь можно добавить другие действия после затемнения
+        // Например, показать меню поражения
     }
 }
